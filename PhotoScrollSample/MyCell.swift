51c121a65cde3ecf45dd993f2adb049cdfd976f5
NAKAGAKI Kenji <nakaken0629@gmail.com>
2016-02-18 08:13:54 +0900

    ルートのテーブルを作成

---
+++ PhotoScrollSample/MyCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCell.swift
+//  PhotoScrollSample
+//
+//  Created by 中垣 健志 on 2016/02/18.
+//  Copyright © 2016年 itvirtuoso. All rights reserved.
+//
+
+import UIKit
+
+class MyCell : UITableViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var photo2Image: UIImageView!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+}
--- PhotoScrollSample/ViewController.swift
+++ PhotoScrollSample/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    private let items = ["アイテム1", "アイテム2", "アイテム3"]
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = self.tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as! MyCell
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("MyCell") as! MyCell
+        cell.photoImage.image = UIImage(named: "Ateam")
+        cell.photo2Image.image = UIImage(named: "Ateam")
+        cell.commentLabel.text = self.items[indexPath.item]
+        return cell
+    }
 }
 
